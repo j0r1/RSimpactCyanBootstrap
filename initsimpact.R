80b0adcc8703fa2897f86a28cc7bb7b013d15782
Jori Liesenborgs <jori.liesenborgs@gmail.com>
2016-10-29 20:38:51 +0200

    No longer specifying 'repos' in install.packages
    Instead, the default repository list is temporarily changed: out own
    repository is added to it. Because of this, specifying 'repos' in
    'install.packages' is no longer required.
    
    For the same reason, the 'Repository' column is no longer needed in
    the CSV file.

--- initsimpact.R
+++ initsimpact.R
@@ -1,54 +1,67 @@
 local({
 
-    packagesUrl <- "https://raw.githubusercontent.com/j0r1/RSimpactCyanBootstrap/master/packages.csv"
+    origRepos <- getOption("repos")
     ownRepos <- "http://research.edm.uhasselt.be/jori"
-    defaultRepos <- getOption("repos")[["CRAN"]]
-    if (defaultRepos == "@CRAN@") {
-        defaultRepos <- "https://cran.ma.imperial.ac.uk/"
-    }
-
-    installPackages <- function(packageInfo) {
-
-        for (name in names(packageInfo)) {
-            tryCatch({ 
-                library(name, character.only=TRUE)
-                vPack <- toString(packageVersion(name))
-                vNeeded <- packageInfo[[name]][2]
-
-                message("Version for ", name, " is ", vPack, ", needed version is ", vNeeded)
-                if (utils::compareVersion(vPack, vNeeded) < 0) {
-                    message("Package ", name, " is outdated, updating")
-                    install.packages(name, repos=packageInfo[name])
-                }
-            }, error = function(e) { # We're assuming that it can't be loaded because it doesn't exist
-                print(e)
-                message("Installing package ", name, " for the first time")
-                install.packages(name, repos=packageInfo[[name]])
-            })
-            library(name, character.only=TRUE)
-        }
-    }
-
-    packageInfo <- list()
-    packageInfo[["RCurl"]] <- c(defaultRepos, "0.0.0")
-    installPackages(packageInfo)
+    defaultRepos <- "https://cran.ma.imperial.ac.uk/"
+    packagesUrl <- "https://raw.githubusercontent.com/j0r1/RSimpactCyanBootstrap/master/packages.csv"
     
-    library(RCurl)
-    csvInfo <- read.csv(text = getURL(packagesUrl))
-    packageInfo <- list()
+    tryCatch({
 
-    for (i in 1:nrow(csvInfo)) {
-        n <- toString(csvInfo$Name[[i]])
-        v <- toString(csvInfo$Version[[i]])
-        r <- toString(csvInfo$Repository[[i]])
-        if (r == "own") {
-            r <- ownRepos
-        } else if (r == "default") {
-            r <- defaultRepos
+        # Add own repository to the list, and if no default CRAN
+        # repo has been provided, use a specific mirror
+        x <- getOption("repos")
+        if (!is.element("CRAN", x)) { 
+            x["CRAN"] = defaultRepos
+        } else {
+            if (x["CRAN"] == "@CRAN@") { # Use a default repository instead, this value would prompt user interaction
+                x["CRAN"] = defaultRepos
+            }
         }
-        packageInfo[[n]] <- c(r, v)
-    }
+        
+        x["SimpactCyan"] <- "http://research.edm.uhasselt.be/jori"
+        options(repos = x)
 
-    installPackages(packageInfo)
+        # Helper function to install specified packages
+        installPackages <- function(packageInfo) {
+    
+            for (name in names(packageInfo)) {
+                tryCatch({ 
+                    library(name, character.only=TRUE)
+                    vPack <- toString(packageVersion(name))
+                    vNeeded <- packageInfo[[name]]
+    
+                    message("Version for ", name, " is ", vPack, ", needed version is ", vNeeded)
+                    if (utils::compareVersion(vPack, vNeeded) < 0) {
+                        message("Package ", name, " is outdated, updating")
+                        install.packages(name)
+                    }
+                }, error = function(e) { # We're assuming that it can't be loaded because it doesn't exist
+                    print(e)
+                    message("Installing package ", name, " for the first time")
+                    install.packages(name)
+                })
+                library(name, character.only=TRUE)
+            }
+        }
+    
+        packageInfo <- list()
+        packageInfo[["RCurl"]] <- "0.0.0"
+        installPackages(packageInfo)
+        
+        library(RCurl)
+        csvInfo <- read.csv(text = getURL(packagesUrl))
+        packageInfo <- list()
+    
+        for (i in 1:nrow(csvInfo)) {
+            n <- toString(csvInfo$Name[[i]])
+            v <- toString(csvInfo$Version[[i]])
+            packageInfo[[n]] <- v
+        }
+    
+        installPackages(packageInfo)
+    }, finally =  {
+        # Restore the original repository settings
+	options(repos = origRepos)
+    })
 })
 
